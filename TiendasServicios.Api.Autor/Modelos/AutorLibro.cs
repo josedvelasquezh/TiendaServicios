c49ffdab31572e3f89c10ddecac35c7549416bef
J. Daniel Velasquez <jvelasquez@thefactoryhka.com>
2021-02-26 09:21:07 -0500

    nuevas clases

---
+++ TiendasServicios.Api.Autor/Modelos/AutorLibro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendasServicios.Api.Autor.Modelos
+{
+    public class AutorLibro
+    {
+        public int AutorLibroId { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public ICollection<GradoAcademico> ListaGradoAcademico { get; set; }
+        public string AutorLibroGuid { get; set; }
+    }
+}
---
+++ TiendasServicios.Api.Autor/Modelos/GradoAcademico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendasServicios.Api.Autor.Modelos
+{
+    public class GradoAcademico
+    {
+        public int GradoAcademicoId { get; set; }
+        public String Nombre { get; set; }
+        public String CentroAcademico { get; set; }
+        public DateTime? FechaGrado { get; set; }
+        public int AutorLibroId { get; set; }
+        public AutorLibro AutorLibro { get; set; }
+        public string GradoAcademicoGuid { get; set; }
+    }
+}
